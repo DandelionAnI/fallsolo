0572b9a233706e809f80504932f582a3e40d1189
YYZ <yuyuezhang0550@163.com>
2021-07-14 23:08:24 +0800

    7.14

--- solution/31nextPermutation.go
+++ solution/31nextPermutation.go
@@ -3,13 +3,13 @@ package solution
 //双指针 on o1
 func nextPermutation(nums []int) {
 	n := len(nums) - 1
-	l := n - 1
+	l := n - 1	
 	for l >= 0 && nums[l] >= nums[l+1] {
 		l--
 	}
 	if l >= 0 {
 		r := n
-		for r >= 0 && nums[l] >= nums[r] {
+		for r >= 0 && nums[l] >= nums[r] {  
 			r--
 		}
 		nums[l], nums[r] = nums[r], nums[l]
