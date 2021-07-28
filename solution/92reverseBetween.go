26445c4e91d369f129f28a7eb7fde36a9754fa64
yuyuezhang <yuyuezhang0550@163.com>
2021-07-29 03:46:06 +0800

    7.28

---
+++ solution/92reverseBetween.go
@@ -0,0 +1,36 @@
+package solution
+
+//头插法 on o1
+func reverseBetween(head *ListNode, left, right int) *ListNode {
+	hair := &ListNode{Val: 0, Next: head}
+	pre := hair
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+	cur := pre.Next
+	for i := 0; i < right-left; i++ {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+	return hair.Next
+}
+
+func reverseBetween(head *ListNode, left, right int) *ListNode {
+	// 设置 dummyNode 是这一类问题的一般做法
+	dummyNode := &ListNode{Val: -1}
+	dummyNode.Next = head
+	pre := dummyNode
+	for i := 0; i < left-1; i++ {
+		pre = pre.Next
+	}
+	cur := pre.Next
+	for i := 0; i < right-left; i++ {
+		next := cur.Next
+		cur.Next = next.Next
+		next.Next = pre.Next
+		pre.Next = next
+	}
+	return dummyNode.Next
+}
