1c0d98c5a5e4dcce96d7e29fbd930faf489a9ede
yuyuezhang <yuyuezhang0550@163.com>
2021-08-15 19:47:33 +0800

    8.15

---
+++ string_go.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func string_go() {
+	fmt.Println("查找子串是否在指定的字符串中")
+	fmt.Println(" Contains 函数的用法")
+	fmt.Println(strings.Contains("seafood", "foo")) //true
+	fmt.Println(strings.Contains("seafood", "bar")) //false
+	fmt.Println(strings.Contains("seafood", ""))    //true
+	fmt.Println(strings.Contains("", ""))           //true 这里要特别注意
+	fmt.Println(strings.Contains("我是中国人", "我"))     //true
+
+	fmt.Println("")
+	fmt.Println(" ContainsAny 函数的用法")
+	fmt.Println(strings.ContainsAny("team", "i"))        // false
+	fmt.Println(strings.ContainsAny("failure", "u & i")) // true
+	fmt.Println(strings.ContainsAny("foo", ""))          // false
+	fmt.Println(strings.ContainsAny("", ""))             // false
+
+	fmt.Println("")
+	fmt.Println(" ContainsRune 函数的用法")
+	fmt.Println(strings.ContainsRune("我是中国", '我')) // true 注意第二个参数，用的是字符
+
+	fmt.Println("")
+	fmt.Println(" Count 函数的用法")
+	fmt.Println(strings.Count("cheese", "e")) // 3
+	fmt.Println(strings.Count("five", ""))    // before & after each rune result: 5 , 源码中有实现
+
+	fmt.Println("")
+	fmt.Println(" Index 函数的用法")
+	fmt.Println(strings.Index("NLT_abc", "abc")) // 返回第一个匹配字符的位置，这里是4
+	fmt.Println(strings.Index("NLT_abc", "aaa")) // 在存在返回 -1
+	fmt.Println(strings.Index("我是中国人", "中"))     // 在存在返回 6
+
+	fmt.Println("")
+	fmt.Println(" IndexAny 函数的用法")
+	fmt.Println(strings.IndexAny("我是中国人", "中")) // 在存在返回 6
+	fmt.Println(strings.IndexAny("我是中国人", "和")) // 在存在返回 -1
+
+	fmt.Println("")
+	fmt.Println(" Index 函数的用法")
+	fmt.Println(strings.IndexRune("NLT_abc", 'b')) // 返回第一个匹配字符的位置，这里是4
+	fmt.Println(strings.IndexRune("NLT_abc", 's')) // 在存在返回 -1
+	fmt.Println(strings.IndexRune("我是中国人", '中'))   // 在存在返回 6
+
+	fmt.Println("")
+	fmt.Println(" Repeat 函数的用法")
+	fmt.Println("ba" + strings.Repeat("na", 2)) //banana
+
+	fmt.Println("")
+	fmt.Println(" Replace 函数的用法")
+	fmt.Println(strings.Replace("oink oink oink", "k", "ky", 2))
+	fmt.Println(strings.Replace("oink oink oink", "oink", "moo", -1))
+
+	fmt.Println("")
+	fmt.Println(" Split 函数的用法")
+	fmt.Printf("%q\n", strings.Split("a,b,c", ","))
+	fmt.Printf("%q\n", strings.Split("a man a plan a canal panama", "a "))
+	fmt.Printf("%q\n", strings.Split(" xyz ", ""))
+	fmt.Printf("%q\n", strings.Split("", "Bernardo O'Higgins"))
+
+}
